decb2cceeac91dd4300fcaaef0c05c86af89d49f
imtheKhan <ikhan1057@gmail.com>
2022-06-17 22:45:06 +0530

    Second Commit

--- app/src/main/java/com/weatherapp/data/WeatherDatabase.kt
+++ app/src/main/java/com/weatherapp/data/WeatherDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.weatherapp.model.Favorite
 import com.weatherapp.model.WeatherUnit
 
-@Database(entities = [Favorite::class,WeatherUnit::class], exportSchema = false, version = 2)
+@Database(entities = [Favorite::class,WeatherUnit::class], exportSchema = false, version = 1)
 abstract class WeatherDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
